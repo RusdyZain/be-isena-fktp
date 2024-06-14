6288131a6be83eaf0d9bc0d640d32b5ac21be22b
RusdyZain <ryanfajri28@gmail.com>
2024-06-14 17:09:03 +0800

    Progress

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(
   cors({
     credentials: true,
     // origin: "http://localhost:5173",
-    origin: "https://isena-fktp.vercel.app/",
+    origin: "https://isena-fktp.vercel.app",
   })
 );
 
