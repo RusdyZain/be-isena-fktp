c444a8a14c2001e70397bbaacdb9d385f3b7c570
RusdyZain <ryanfajri28@gmail.com>
2024-06-14 17:04:05 +0800

    Progress

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(
     store: store,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
-      secure: false,
+      secure: true,
       sameSite: "none",
       httpOnly: true,
     },
@@ -58,7 +58,8 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://isena-fktp.vercel.app/",
   })
 );
 
