9b01f1ee0ee276dc3f3d1cb936652d8b5412457e
RusdyZain <ryanfajri28@gmail.com>
2024-06-14 18:18:16 +0800

    Progress 4

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -1,94 +1,130 @@
+import jwt from "jsonwebtoken";
 import Users from "../models/UserModel.js";
-import jwt from 'jsonwebtoken';
 
-export const verifyUser = async (req, res, next) =>{
-    if(!req.session.userId){
-        return res.status(401).json({msg: "Please login in your account!"});
+export const verifyUser = async (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ msg: "Please login in your account!" });
+  }
+
+  const token = authorizationHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await Users.findOne({
+      where: {
+        uuid: decoded.uuid,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
     }
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
+
     req.userId = user.id;
-    req.role = user.role; 
+    req.role = user.role;
     next();
-}
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return res.status(401).json({ msg: "Invalid or expired token" });
+  }
+};
 
-export const klinikOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "admin" && user.role !== "dokter") return res.status(403).json({msg: "Silahkan Login Menggunakan User Admin atau Dokter"});
-    next();
-}
+export const klinikOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "admin" && user.role !== "dokter")
+    return res
+      .status(403)
+      .json({ msg: "Silahkan Login Menggunakan User Admin atau Dokter" });
+  next();
+};
 
-export const dokterOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "dokter") return res.status(403).json({msg: "Silahkan Login Menggunakan User Dokter"});
-    next();
-}
+export const dokterOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "dokter")
+    return res
+      .status(403)
+      .json({ msg: "Silahkan Login Menggunakan User Dokter" });
+  next();
+};
 
-export const adminOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "admin") return res.status(403).json({msg: "Silahkan Login Menggunakan User Admin"});
-    next();
-}
+export const adminOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "admin")
+    return res
+      .status(403)
+      .json({ msg: "Silahkan Login Menggunakan User Admin" });
+  next();
+};
 
-export const apotekerOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "apoteker") return res.status(403).json({msg: "Silahkan Login Menggunakan User Dokter dan Apoteker"});
-    next();
-}
+export const apotekerOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "apoteker")
+    return res
+      .status(403)
+      .json({ msg: "Silahkan Login Menggunakan User Dokter dan Apoteker" });
+  next();
+};
 
-export const pawasOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
+export const pawasOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "pawas")
+    return res.status(403).json({
+      msg: "Silahkan Login Menggunakan User Perwira Pengawas (PAWAS)",
     });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "pawas") return res.status(403).json({msg: "Silahkan Login Menggunakan User Perwira Pengawas (PAWAS)"});
-    next();
-}
+  next();
+};
 
-export const statistikOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "statistik") return res.status(403).json({msg: "Silahkan Login Menggunakan User Statistik"});
-    next();
-}
+export const statistikOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "statistik")
+    return res
+      .status(403)
+      .json({ msg: "Silahkan Login Menggunakan User Statistik" });
+  next();
+};
 
-export const apotekOnly = async (req, res, next) =>{
-    const user = await Users.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    if(user.role !== "apoteker" && user.role !== "dokter") return res.status(403).json({msg: "Silahkan Login Menggunakan User Apoteker"});
-    next();
-}
+export const apotekOnly = async (req, res, next) => {
+  const user = await Users.findOne({
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  if (user.role !== "apoteker" && user.role !== "dokter")
+    return res
+      .status(403)
+      .json({ msg: "Silahkan Login Menggunakan User Apoteker" });
+  next();
+};
--- routes/PasienRoute.js
+++ routes/PasienRoute.js
@@ -1,20 +1,19 @@
 import express from "express";
 import {
-    getPasiens,
-    getPasienById,
-    createPasien,
-    updatePasien,
-    deletePasien
+  getPasiens,
+  getPasienById,
+  createPasien,
+  updatePasien,
+  deletePasien,
 } from "../controllers/Pasiens.js";
-import { verifyUser, adminOnly, klinikOnly } from "../middleware/AuthUser.js";
-
+import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/pasiens', verifyUser,  getPasiens);
-router.get('/pasiens/:id', verifyUser,   getPasienById);
-router.post('/pasiens', verifyUser, adminOnly,  createPasien);
-router.patch('/pasiens/:id', verifyUser, adminOnly,  updatePasien);
-router.delete('/pasiens/:id', verifyUser, adminOnly,  deletePasien);
+router.get("/pasiens", verifyUser, getPasiens);
+router.get("/pasiens/:id", verifyUser, getPasienById);
+router.post("/pasiens", verifyUser, adminOnly, createPasien);
+router.patch("/pasiens/:id", verifyUser, adminOnly, updatePasien);
+router.delete("/pasiens/:id", verifyUser, adminOnly, deletePasien);
 
 export default router;
