f693cd27f4f98a5070cb9d98019d1e6ad9956bb0
RusdyZain <ryanfajri28@gmail.com>
2024-06-16 02:02:17 +0800

    Progress 12

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -1,9 +1,6 @@
 import jwt from "jsonwebtoken";
 import Users from "../models/UserModel.js";
 
-import jwt from "jsonwebtoken";
-import Users from "../models/UserModel.js"; // Sesuaikan dengan path model User Anda
-
 export const verifyUser = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
