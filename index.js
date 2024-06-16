54dce3044156d46938ae06a4ed9c69c97cf544ae
RusdyZain <ryanfajri28@gmail.com>
2024-06-16 16:14:55 +0800

    Progress 21

--- index.js
+++ index.js
@@ -53,6 +53,17 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://isena-fktp.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // 3. Set up session middleware
 app.use(
   session({
