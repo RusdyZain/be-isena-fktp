04f6c67e83bb5aa23b65594ce5aa8f247c7fb0b6
RusdyZain <ryanfajri28@gmail.com>
2024-07-13 23:24:39 +0800

    Progress 43

--- routes/pasien/data_statistik/TotalPenyakitRoute.js
+++ routes/pasien/data_statistik/TotalPenyakitRoute.js
@@ -3,6 +3,7 @@ import {
   getTotalPenyakits,
   deleteTotalPenyakit,
 } from "../../../controllers/pasien/data_statistik/Totalpenyakits.js";
+
 import { statistikOnly } from "../../../middleware/userOnly.js";
 
 const router = express.Router();
