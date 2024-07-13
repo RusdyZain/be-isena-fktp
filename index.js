b6ac1de9c375f97d94da218fbbf1e215a29e410d
RusdyZain <ryanfajri28@gmail.com>
2024-07-13 23:27:52 +0800

    Progress 434

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ import PemeriksaanRoute from "./routes/pasien/dokter/datakunjungan/PemeriksaanRo
 import RiwayatalergiRoute from "./routes/pasien/dokter/datakunjungan/RiwayatalergiRoute.js";
 import TandatanganRoute from "./routes/pasien/dokter/datakunjungan/TandatanganRoute.js";
 
-import TotalpenyakitRoute from "./routes/pasien/data_statistik/TotalpenyakitRoute.js";
+import TotalpenyakitRoute from "./routes/pasien/data_statistik/TotalPenyakitRoute.js";
 
 import PegawaiRoute from "./routes/pegawai/PegawaiRoute.js";
 import AuthPegawaiRoute from "./routes/pegawai/AuthPegawaiRoute.js";
